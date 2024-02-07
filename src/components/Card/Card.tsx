880a750b3700585dec522fb0913e9b4455151e50
AttilaMuller <midension@gmail.com>
2024-02-07 21:17:30 +0100

    Change identation

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -2,22 +2,22 @@ import React from 'react';
 import './Card.scss';
 
 export const Card = () => {
-  return (
-    <div className='card'>
-      <div>
-        <span className='card-title'>Pokemon name</span>
-        <span className='card-title-votes'>12</span>
-      </div>
-      <img className='card-image' src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png' />
-      <div className='card-types'>
-        <span className='card-static-button'>Type 1</span>
-        <span className='card-static-button'>Type 2</span>
-      </div>
-      <div className='card-abilities'>
-        <span className='card-static-button'>Ability 1</span>
-        <span className='card-static-button'>Ability 2</span>
-        <span className='card-static-button'>Ability 3</span>
-      </div>
-    </div>
-  )
+    return (
+        <div className='card'>
+            <div>
+                <span className='card-title'>Pokemon name</span>
+                <span className='card-title-votes'>12</span>
+            </div>
+            <img className='card-image' src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png' />
+            <div className='card-types'>
+                <span className='card-static-button'>Type 1</span>
+                <span className='card-static-button'>Type 2</span>
+            </div>
+            <div className='card-abilities'>
+                <span className='card-static-button'>Ability 1</span>
+                <span className='card-static-button'>Ability 2</span>
+                <span className='card-static-button'>Ability 3</span>
+            </div>
+        </div>
+    )
 }
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -4,17 +4,17 @@ import './NavBar.scss';
 import { Link } from 'react-router-dom';
 
 export const NavBar = () => {
-  return (
-    <div className='navbar'>
-      <img className='navbar-logo' src={logo} alt='logo' />
-      <div className='navbar-options'>
-        <div className='navbar-options-item'>
-          <Link to='/'>Voting</Link>
+    return (
+        <div className='navbar'>
+            <img className='navbar-logo' src={logo} alt='logo' />
+            <div className='navbar-options'>
+                <div className='navbar-options-item'>
+                    <Link to='/'>Voting</Link>
+                </div>
+                <div className='navbar-options-item'>
+                    <Link to='/top-ten'>Top 10</Link>
+                </div>
+            </div>
         </div>
-        <div className='navbar-options-item'>
-          <Link to='/top-ten'>Top 10</Link>
-        </div>
-      </div>
-    </div>
-  )
+    )
 }
